4ad3b11dca2b50d019ce6b147ac7edc3f57ee3c5
mskim1624 <mskim1624@naver.com>
2023-02-20 11:11:45 +0900

    0220 - 2

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,114 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float speed;
+    public bool isTouchTop = false;
+    public bool isTouchBottom = false;
+    public bool isTouchRight = false;
+    public bool isTouchLeft = false;
+
+    Animator anim;
+
+    public GameObject bulletPrefab;
+    public float curBulletDelay = 0f;
+    public float maxBulletDelay = 1f;
+
+    private void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+        Fire();
+        ReloadBullet();
+    }
+
+    void Move()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        if ((isTouchRight && h == 1) || (isTouchLeft && h == -1))
+        {
+            h = 0;
+        }
+        if ((isTouchTop && v == 1) || (isTouchBottom && v == -1))
+        {
+            v = 0;
+        }
+
+        Vector3 curPos = transform.position;
+        Vector3 nextPos = new Vector3(h, v, 0) * speed * Time.deltaTime;
+
+        transform.position = curPos + nextPos;
+
+        anim.SetInteger("Input", (int)h);
+    }
+
+    void Fire()
+    {
+        if (!Input.GetButton("Fire1"))
+            return;
+
+        if (curBulletDelay < maxBulletDelay)
+            return;
+
+        GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+        Rigidbody2D rd = bullet.GetComponent<Rigidbody2D>();
+        rd.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+
+        curBulletDelay = 0;
+    }
+
+    void ReloadBullet()
+    {
+        curBulletDelay += Time.deltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "PlayerBorder")
+        {
+            switch(collision.gameObject.name)
+            {
+                case "Top":
+                    isTouchTop = true;
+                    break;
+                case "Bottom":
+                    isTouchBottom = true;
+                    break;
+                case "Right":
+                    isTouchRight = true;
+                    break;
+                case "Left":
+                    isTouchLeft = true;
+                    break;
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "PlayerBorder")
+        {
+            switch (collision.gameObject.name)
+            {
+                case "Top":
+                    isTouchTop = false;
+                    break;
+                case "Bottom":
+                    isTouchBottom = false;
+                    break;
+                case "Right":
+                    isTouchRight = false;
+                    break;
+                case "Left":
+                    isTouchLeft = false;
+                    break;
+            }
+        }
     }
 }
